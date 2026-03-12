4d7bc7d074bc29a757e3eb8403d6bc7b918543b7
romaleks <romanov1371@gmail.com>
2026-03-12 19:20:23 +0300

    Refactor blog API to use async/await, add validation, and implement CRUD operations with tests

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,18 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
+
+  const blog = new Blog(body)
+
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  if (!body.likes) {
+    return response.status(400).json({ error: 'likes are required' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) return response.status(404).end()
+  blog.likes = body.likes
+
+  const result = await blog.save()
+  response.json(result)
 })
 
 module.exports = blogsRouter
---
+++ backend/tests/blog_api.test.js
@@ -0,0 +1,143 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('unique identifier property of the blog posts is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach((blog) => {
+      assert.ok(blog.id, 'Blog should have an id property')
+      assert.strictEqual(typeof blog.id, 'string', 'id should be a string')
+      assert.ok(!blog._id, 'Blog should not have _id property')
+      assert.ok(!blog.__v, 'Blog should not have __v property')
+    })
+  })
+
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'New Blog Post',
+        author: 'John Doe',
+        url: 'http://example.com/new-blog-post',
+        likes: 5,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+      const titles = blogsAtEnd.map((b) => b.title)
+      assert.ok(
+        titles.includes('New Blog Post'),
+        'New blog title should be saved',
+      )
+    })
+
+    test('sets a value of "likes" property to 0 if it is missing', async () => {
+      const newBlog = {
+        title: 'New Blog Post',
+        author: 'John Doe',
+        url: 'http://example.com/new-blog-post',
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(response.body.likes, 0)
+    })
+
+    test('fails with status code 400 if data invalid', async () => {
+      const newBlog1 = {
+        author: 'John Doe',
+        url: 'http://example.com/new-blog-post',
+        likes: 5,
+      }
+
+      const newBlog2 = {
+        title: 'New Blog Post',
+        author: 'John Doe',
+        likes: 5,
+      }
+
+      await api.post('/api/blogs').send(newBlog1).expect(400)
+      await api.post('/api/blogs').send(newBlog2).expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      const ids = blogsAtEnd.map((n) => n.id)
+      assert(!ids.includes(blogToDelete.id))
+
+      assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+    })
+  })
+
+  describe('updation of a blog', () => {
+    test('succeeds with status code 204 if id and data is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+
+      const updatedBlog = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send({ likes: 5 })
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(updatedBlog.body.likes, 5)
+    })
+
+    test('fails with status code 400 if "likes" is missing', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+
+      await api.put(`/api/blogs/${blogToUpdate.id}`).send({}).expect(400)
+    })
+  })
+})
+after(async () => {
+  await mongoose.connection.close()
+})
